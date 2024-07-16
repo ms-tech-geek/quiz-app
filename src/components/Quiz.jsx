d44c6adec8dc23bebd99494a66ab97b7f5f64448
Mayank Sethi <mayank@digibrain.co.in>
2024-07-16 18:03:05 +0530

    create quiz component with deriving values, outputting questions and registering answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react';
-
+import QUESTIONS from '../questions';
 const Quiz = () => {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  const handleSelectAnswer = (selectedAnswer) => {
+    setUserAnswers((prevUserAnswers) => [...prevUserAnswers, selectedAnswer]);
+  };
+
   return (
     <main>
-      <p>Currently active question</p>
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      </div>
+      <ul id="answers">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+          <li key={index} className="answer">
+            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
