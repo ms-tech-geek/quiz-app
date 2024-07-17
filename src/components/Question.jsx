891041575056c0a80b0791c00b05544ae2145b67
Mayank Sethi <mayank@digibrain.co.in>
2024-07-17 15:43:52 +0530

    clean up code

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,23 +1,46 @@
 import QuestionTimer from './QuestionTimer';
 import Answers from './Answers';
+import QUESTIONS from '../questions.js';
+import { useState } from 'react';
 
-const Question = ({
-  questionText,
-  answers,
-  onSelectAnswer,
-  selectedAnswer,
-  answerState,
-  onSkipAnswer,
-}) => {
+const Question = ({ questionIndex, onSelectAnswer, onSkipAnswer }) => {
+  const [answer, setAnswer] = useState({
+    selectedAnswer: '',
+    isCorrect: null,
+  });
+
+  const handleSelectAnswer = (answer) => {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: QUESTIONS[questionIndex].answers[0] === answer,
+    });
+
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: true,
+      });
+
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  };
+
+  let answerState = '';
+  console.log(`selectedAnswer`, answer.selectedAnswer);
+  if (answer.selectedAnswer) {
+    answerState = answer.isCorrect ? 'correct' : 'wrong';
+  }
   return (
     <div id="question">
       <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
-      <h2>{questionText}</h2>
+      <h2>{QUESTIONS[questionIndex].text}</h2>
       <Answers
-        answers={answers}
-        selectedAnswer={selectedAnswer}
+        answers={QUESTIONS[questionIndex].answers}
+        selectedAnswer={answer.selectedAnswer}
         answerState={answerState}
-        onSelect={onSelectAnswer}
+        onSelect={handleSelectAnswer}
       />
     </div>
   );
