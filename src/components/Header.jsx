136f2867cb15bb5f185d5e3a8541d560f5518c5f
Mayank Sethi <mayank@digibrain.co.in>
2024-07-22 15:44:31 +0530

    header clean up for styling;

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,16 @@
 // src/components/Header.js
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import appLogo from '../assets/quiz-logo.png';
 import './Header.css';
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <header className="header">
       <div className="header-container">
         <div className="logo">
-          <Link to="/">
-            <img src={appLogo} alt="Logo" />{' '}
-          </Link>
+          <img src={appLogo} alt="Logo" />{' '}
         </div>
-        <div className="menu-icon" onClick={toggleMenu}>
-          <div className={isOpen ? 'menu-icon-open' : 'menu-icon-closed'}>
-            <div className="bar1"></div>
-            <div className="bar2"></div>
-            <div className="bar3"></div>
-          </div>
-        </div>
-        <nav className={isOpen ? 'nav-menu open' : 'nav-menu'}>
+        <nav className={'nav-menu'}>
           <Link to="/" className="nav-item">
             Home
           </Link>
