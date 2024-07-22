4abae1a2a6ad5f94b0b25ef520130c4281df6688
Mayank Sethi <mayank@digibrain.co.in>
2024-07-22 12:41:56 +0530

    update content for home page to bollywood quiz app

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,11 @@
 import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import './Home.css';
+import './Home.css'; // Ensure you have this CSS file for styling
 
 const Home = () => {
-  const navigate = useNavigate();
-
-  // const startQuiz = () => {
-  //   navigate('/quiz'); // Navigate to the quiz page
-  // };
-
   return (
     <div className="home-container">
       <header className="home-header">
-        <h1>Quiz App</h1>
+        <h1>Bollywood</h1>
         <nav>
           <Link to="/quiz">
             <button className="home-button">Start Quiz</button>
@@ -21,10 +14,10 @@ const Home = () => {
       </header>
       <main>
         <section className="home-welcome">
-          <h2>Discover Your Quiz Persona Today!</h2>
+          <h2>Discover Your Bollywood Knowledge!</h2>
           <p>
-            Welcome to the Quiz App, where understanding your natural quiz
-            persona just got easier.
+            Welcome to the Bollywood Quiz App, where you can test your knowledge
+            of Bollywood movies, songs, and dialogues.
           </p>
           <Link to="/quiz">
             <button className="start-quiz-button">Take the Quiz</button>
@@ -33,23 +26,14 @@ const Home = () => {
         <section className="home-info">
           <h3>Why Take This Quiz?</h3>
           <p>
-            Ever wonder why you excel in some areas more than others? Take this
-            quiz to find out!
+            Ever wonder how well you know Bollywood? Take this quiz to find out!
           </p>
-          <h3>What You Need to Know</h3>
+          <h3>What You Can Do</h3>
           <ul>
-            <li>
-              No Advertisements: Our quiz is straightforward and ad-free for an
-              uninterrupted experience.
-            </li>
-            <li>
-              Scientifically Backed: Developed with insights from experts to
-              ensure accuracy and relevance.
-            </li>
-            <li>
-              Immediate Results: Get instant feedback on your quiz persona at
-              the end of the quiz.
-            </li>
+            <li>Guess the movie based on a famous dialogue.</li>
+            <li>Guess the movie based on a popular song.</li>
+            <li>Guess the movie based on a movie scene image.</li>
+            <li>Guess the song based on a video clip.</li>
           </ul>
         </section>
       </main>
