83d9081c3d1753044cbe8bd1db076f3ca2782932
Mayank Sethi <mayank@digibrain.co.in>
2024-07-22 11:46:28 +0530

    add react router for navigation between quiz page and landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import Header from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+
 import Quiz from './components/Quiz';
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Quiz />
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+
+          <Route path="/quiz" element={<Quiz />} />
+        </Routes>
+      </Router>
     </>
   );
 };
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,14 +1,21 @@
 import { useState, useCallback } from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import Header from './Header';
 import combinedQuestions from '../questions.js';
 import Question from './Question.jsx';
 import Summary from './Summary.jsx';
 
 export default function Quiz() {
+  const navigate = useNavigate();
+
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === combinedQuestions.length;
+
+  const handleHomeClick = () => {
+    navigate('/'); // Navigate back to the landing page
+  };
 
   const handleSelectAnswer = useCallback(function handleSelectAnswer(
     selectedAnswer
@@ -29,13 +36,17 @@ export default function Quiz() {
   }
 
   return (
-    <div id="quiz">
-      <Question
-        key={activeQuestionIndex}
-        questionIndex={activeQuestionIndex}
-        onSelectAnswer={handleSelectAnswer}
-        onSkipAnswer={handleSkipAnswer}
-      />
-    </div>
+    <>
+      <Header />
+      <button onClick={handleHomeClick}>Home</button>
+      <div id="quiz">
+        <Question
+          key={activeQuestionIndex}
+          questionIndex={activeQuestionIndex}
+          onSelectAnswer={handleSelectAnswer}
+          onSkipAnswer={handleSkipAnswer}
+        />
+      </div>
+    </>
   );
 }
