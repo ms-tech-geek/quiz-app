83f72caa34bea4fa52a6ab534a8abe3b5b54c29f
Mayank Sethi <mayank@digibrain.co.in>
2024-07-22 12:59:58 +0530

    restyle

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,21 @@
+// src/components/Home.js
+import React from 'react';
 import { Link } from 'react-router-dom';
-import './Home.css'; // Ensure you have this CSS file for styling
+import './Home.css';
 
 const Home = () => {
   return (
     <div className="home-container">
       <header className="home-header">
-        <h1>Bollywood</h1>
-        <nav>
-          <Link to="/quiz">
-            <button className="home-button">Start Quiz</button>
-          </Link>
-        </nav>
+        <h1>Bollywood Quiz App</h1>
       </header>
       <main>
         <section className="home-welcome">
-          <h2>Discover Your Bollywood Knowledge!</h2>
+          <h2>Test Your Bollywood Knowledge!</h2>
           <p>
-            Welcome to the Bollywood Quiz App, where you can test your knowledge
-            of Bollywood movies, songs, and dialogues.
+            Welcome to the Bollywood Quiz App, where you can guess movies,
+            songs, dialogues, and scenes. Challenge yourself and see how well
+            you know Bollywood!
           </p>
           <Link to="/quiz">
             <button className="start-quiz-button">Take the Quiz</button>
@@ -26,14 +24,31 @@ const Home = () => {
         <section className="home-info">
           <h3>Why Take This Quiz?</h3>
           <p>
-            Ever wonder how well you know Bollywood? Take this quiz to find out!
+            Think you're a Bollywood buff? Put your knowledge to the test and
+            prove your movie mastery with our exciting quiz!
           </p>
+        </section>
+        <section className="home-info">
           <h3>What You Can Do</h3>
           <ul>
             <li>Guess the movie from a legendary dialogue.</li>
             <li>Identify the movie from a hit song.</li>
             <li>Recognize the movie from an iconic scene image.</li>
-            <li>Name the movie from a memorable video clip.</li>
+            <li>Name the song from a memorable video clip.</li>
+          </ul>
+        </section>
+        <section className="home-features">
+          <h3>Features</h3>
+          <ul>
+            <li>No Advertisements: Enjoy an uninterrupted experience.</li>
+            <li>
+              Scientifically Backed: Developed with insights from Bollywood
+              enthusiasts to ensure accuracy and relevance.
+            </li>
+            <li>
+              Immediate Results: Get instant feedback on your Bollywood
+              knowledge at the end of the quiz.
+            </li>
           </ul>
         </section>
       </main>
