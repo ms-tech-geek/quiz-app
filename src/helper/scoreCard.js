7165a977b5d2f8bbfbab831f720e8f8a131a4971
Mayank Sethi <mayank@digibrain.co.in>
2024-07-24 10:57:19 +0530

    derive score

---
+++ src/helper/scoreCard.js
@@ -0,0 +1,20 @@
+export const deriveScore = ({ selectedQuestions, userAnswers }) => {
+  const skippedAnswers = userAnswers.filter((answer) => answer === null);
+  const correctAnswers = userAnswers.filter(
+    (answer, index) => answer === selectedQuestions[index].correctAnswer
+  );
+
+  const skippedAnswersShare = Math.round(
+    (skippedAnswers.length / userAnswers.length) * 100
+  );
+  const correctAnswersShare = Math.round(
+    (correctAnswers.length / userAnswers.length) * 100
+  );
+  const wrongAnswersShare = 100 - skippedAnswersShare - correctAnswersShare;
+
+  return {
+    skippedAnswersShare,
+    correctAnswersShare,
+    wrongAnswersShare,
+  };
+};
